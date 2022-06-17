6ed7892db234f57e4f368297de391a1fa738c7e8
MestreRHG <38073948+MestreRHG@users.noreply.github.com>
2022-06-17 16:33:05 +0100

    Initial commit

---
+++ src/main/java/net/mestrerhg/spawnpicker/SpawnPicker.java
@@ -0,0 +1,15 @@
+package net.mestrerhg.spawnpicker;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SpawnPicker implements ModInitializer {
+	public static final String MOD_ID = "spawnpicker";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/mestrerhg/spawnpicker/SpawnPickerClient.java
@@ -0,0 +1,10 @@
+package net.mestrerhg.spawnpicker;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpawnPickerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
